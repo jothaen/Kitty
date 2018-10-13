25086a732e11189c8fac2b999324c27c1d6e4bb3
Piotr Kozlowski <piotr.kozlowski.1993@gmail.com>
2018-10-13 11:05:33 +0200

    Initial commit

---
+++ app/src/main/java/com/jothaen/kitty/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jothaen.kitty
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
